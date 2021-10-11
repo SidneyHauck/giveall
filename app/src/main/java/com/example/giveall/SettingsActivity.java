a02cfba769659de785765e54c28e21462227c7cc
Sidney Hauck <haucksidney@gmail.com>
2021-10-11 09:22:35 -0500

    Profile image upload in SettingsActivity to Firebase Storage

--- app/src/main/java/com/example/giveall/SettingsActivity.java
+++ app/src/main/java/com/example/giveall/SettingsActivity.java
@@ -1,17 +1,26 @@
 package com.example.giveall;
 
+
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
+import com.canhub.cropper.CropImageContractOptions;
+import com.canhub.cropper.CropImageOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,8 +29,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.canhub.cropper.CropImageView;
+import com.canhub.cropper.CropImageContract;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -31,8 +44,9 @@ public class SettingsActivity extends AppCompatActivity {
     private String currentUserID;
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
-    private static final int GalleryPick=1;
     private StorageReference UserProfileImagesRef;
+    private String userProfileImageURL, setUserName, setUserAddress;
+    Uri ImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,15 +58,75 @@ public class SettingsActivity extends AppCompatActivity {
         RootRef = FirebaseDatabase.getInstance().getReference();
         UserProfileImagesRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
 
+        Toolbar messageToolbar = findViewById(R.id.msg_toolbar);
+        setSupportActionBar(messageToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        messageToolbar.setTitle("");
+        messageToolbar.setSubtitle("");
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        messageToolbar.setNavigationOnClickListener(v -> SendUserToHomeActivity());
+
         InitializeFields();
 
-        UpdateAccountSettings.setOnClickListener(view -> UpdateSettings());
 
         RetrieveUserInfo();
 
+        CropImageContractOptions options = new CropImageContractOptions(ImageUri, new CropImageOptions());
+        options.setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1,1);
+
+
+        ActivityResultLauncher<CropImageContractOptions> activityResultLauncher =
+                registerForActivityResult(new CropImageContract(), result -> {
+                            if (result.isSuccessful()) {
+                                ImageUri = result.getUriContent();
+                                StorageReference filePath = UserProfileImagesRef.child(currentUserID + ".jpg");
+
+                                filePath.putFile(ImageUri)
+                                        .addOnSuccessListener(taskSnapshot -> {
+                                            Toast.makeText(SettingsActivity.this, "Profile Image uploaded Successfully...", Toast.LENGTH_SHORT).show();
+
+                                            filePath.getDownloadUrl().addOnCompleteListener(task -> {
+                                                String profileImageUrl = task.getResult().toString();
+                                                Log.i("URL",profileImageUrl);
+
+                                                RootRef.child("Users").child(currentUserID).child("image")
+                                                        .setValue(profileImageUrl)
+                                                        .addOnCompleteListener(task1 -> {
+                                                            if (task1.isSuccessful())
+                                                            {
+                                                                Toast.makeText(SettingsActivity.this, "Image save in Database, Successfully...", Toast.LENGTH_SHORT).show();
+                                                                SaveProfileImage(profileImageUrl);
+                                                            }
+                                                            else
+                                                            {
+                                                                String message = task1.getException().toString();
+                                                                Toast.makeText(SettingsActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                                                            }
+                                                        });
+                                            });
+
+                                        })
+                                        .addOnFailureListener(e -> {
+                                            String message = e.toString();
+                                            Toast.makeText(SettingsActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                                        });
+                            }
+                        });
+
+        userProfileImage.setOnClickListener(view -> {
+            activityResultLauncher.launch(options);
+        });
+
+        UpdateAccountSettings.setOnClickListener(view -> UpdateSettings());
     }
 
-
+    private void SaveProfileImage(String profileImageUrl){
+        userProfileImageURL = profileImageUrl;
+    }
 
     private void InitializeFields() {
 
@@ -64,8 +138,8 @@ public class SettingsActivity extends AppCompatActivity {
 
 
     private void UpdateSettings() {
-        String setUserName = userName.getText().toString();
-        String setUserAddress = userAddress.getText().toString();
+        setUserName = userName.getText().toString();
+        setUserAddress = userAddress.getText().toString();
 
         if(TextUtils.isEmpty(setUserName)){
             Toast.makeText(this, "Please write your user name.", Toast.LENGTH_LONG).show();
@@ -73,12 +147,12 @@ public class SettingsActivity extends AppCompatActivity {
 
         if(TextUtils.isEmpty(setUserAddress)){
             Toast.makeText(this, "Please write your pickup address for listings.", Toast.LENGTH_LONG).show();
-        }
-        else{
+        }else{
             HashMap<String, String> profileMap = new HashMap<>();
                 profileMap.put("uid", currentUserID);
                 profileMap.put("name", setUserName);
                 profileMap.put("address", setUserAddress);
+                profileMap.put("image", userProfileImageURL);
 
             RootRef.child("Users").child(currentUserID).setValue(profileMap)
             .addOnCompleteListener(task -> {
@@ -105,6 +179,14 @@ public class SettingsActivity extends AppCompatActivity {
 
                             userName.setText(retrieveUserName);
                             userAddress.setText(retrieveUserAddress);
+                            Picasso.get().load(retrieveProfileImage).into(userProfileImage);
+
+                        }else if((snapshot.exists()) && (snapshot.hasChild("name")) && (snapshot.hasChild("image"))) {
+                            String retrieveUserName = snapshot.child("name").getValue().toString();
+                            String retrieveProfileImage = snapshot.child("image").getValue().toString();
+
+                            userName.setText(retrieveUserName);
+                            Picasso.get().load(retrieveProfileImage).into(userProfileImage);
                         }else if((snapshot.exists()) && (snapshot.hasChild("name")) && (snapshot.hasChild("address"))) {
                             String retrieveUserName = snapshot.child("name").getValue().toString();
                             String retrieveUserAddress = snapshot.child("address").getValue().toString();
@@ -133,5 +215,14 @@ public class SettingsActivity extends AppCompatActivity {
         startActivity(mainIntent);
         finish();
 
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if(keyCode==KeyEvent.KEYCODE_BACK)   {
+            SendUserToHomeActivity();
+        }
+        return true;
     }
 }
